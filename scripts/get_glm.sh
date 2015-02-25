b6137712d1bb6d6ee1bee3a65b2c85723a00e422
Dmitry Gerasin <dg@Dmitrys-MacBook-Air.local>
2015-02-25 18:55:19 -0500

    Fixed the get glm sript.

--- scripts/get_glm.sh
+++ scripts/get_glm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # Install glm
-curl -L http://downloads.sourceforge.net/project/ogl-math/glm-0.9.6.1/glm-0.9.6.1.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fogl-math%2Ffiles%2Fglm-0.9.6.1%2F&ts=1423863325&use_mirror=iweb > `pwd`/glm.tar.gz
-tar -zxvf glm.tar.gz
+curl -L http://sourceforge.net/projects/ogl-math/files/glm-0.9.6.3/glm-0.9.6.3.zip > ./glm.zip
+unzip glm.zip
+
+# Clean up
+rm glm.zip
