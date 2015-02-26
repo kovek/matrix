fb1fbc0c1222bcc7fb589b086e95d44ad6c83590
Kevin Galkov <kevin@galkov.com>
2015-02-25 19:07:47 -0500

    Made a get_plplot script and got the Makefile to include the plplot headers

---
+++ scripts/get_plplot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ] || [ "$(uname)" == "Darwin" ]; then
+	# Install glfw
+	curl -L http://sourceforge.net/projects/plplot/files/plplot/5.10.0%20Source/plplot-5.10.0.tar.gz > ./plplot.tar.gz
+	tar -xvf plplot.tar.gz
+
+    # Cleanup
+    rm plplot.tar.gz
+
+	mv plplot-5.10.0 plplot
+	cd plplot
+	mkdir build
+	cd build
+	cmake -DBUILD_SHARED_LIBS=NO ..
+	make
+    # We now have glfw/build/src/libglfw3.a
+    cp src/libglfw3.a ../../libs/libglfw3.a
+
+	# Get out of build and into the root of the glfw dir
+	cd ..
+	# Get out of glfw dir and into external
+    cd ..
+fi
