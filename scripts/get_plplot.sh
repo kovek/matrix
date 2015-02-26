dea591a6eab15f246e6795e7db94ea4ec8c7803b
Dmitry Gerasin <dg@Dmitrys-MacBook-Air.local>
2015-02-25 19:22:11 -0500

    Made the plplot script make install... didnt want to fight that weirdly designed file structure..

--- scripts/get_plplot.sh
+++ scripts/get_plplot.sh
@@ -9,17 +9,23 @@ if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ] || [ "$(uname)" == "Darwin" ]
     # Cleanup
     rm plplot.tar.gz
 
-	mv plplot-5.10.0 plplot
-	cd plplot
-	mkdir build
-	cd build
-	cmake -DBUILD_SHARED_LIBS=NO ..
-	make
-    # We now have glfw/build/src/libglfw3.a
-    cp src/libglfw3.a ../../libs/libglfw3.a
+    mv plplot-5.10.0 plplot
+    cd plplot
+    mkdir build
+    cd build
+    cmake -DBUILD_SHARED_LIBS=NO ..
+    make
 
-	# Get out of build and into the root of the glfw dir
-	cd ..
-	# Get out of glfw dir and into external
+    # We now have glfw/build/src/libglfw3.a
+    #cp src/libglfw3.a ../../libs/libglfw3.a
+    make install
+
+    # Get out of build and into the root of the glfw dir
+    cd ..
+
+    # ln -s include plplot
+    # ln -s bindings/c++/plstream.h plplot/plstream.h
+
+    # Get out of glfw dir and into external
     cd ..
 fi
