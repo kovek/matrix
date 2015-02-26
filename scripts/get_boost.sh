770a41fe4fe2f84159b227b44516c5f5dc258d50
Dmitry Gerasin <dg@Dmitrys-MacBook-Air.local>
2015-02-25 19:35:02 -0500

    Fixed makefile and boost script

--- scripts/get_boost.sh
+++ scripts/get_boost.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# curl -L http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.zip/download > boost_1_57_0.zip
+curl -L http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.zip/download > boost_1_57_0.zip
+
+unzip boost_157_0.zip
 
 mv boost_1_57_0 boost
 cd boost
